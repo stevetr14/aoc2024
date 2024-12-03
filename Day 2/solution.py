6552091dd5728999e71fed327511ecfc07d12a51
Steven Tran <steven.tran@deswik.com>
2024-12-03 10:00:47 +1000

    Day 2 lame solution

--- Day 2/solution.py
+++ Day 2/solution.py
@@ -1,4 +1,4 @@
-from itertools import pairwise
+from itertools import pairwise, combinations
 
 from utils import parse_input
 
@@ -12,7 +12,6 @@ def part_1():
 
     for line in lines:
         x = [int(c) for c in line.split(" ")]
-        print(x, sorted(x), sorted(x, reverse=True))
 
         if x == sorted(x) or x == sorted(x, reverse=True):
             total += 1
@@ -21,9 +20,7 @@ def part_1():
 
         for a, b in list(pairwise(line.split(" "))):
             diff = abs(int(a) - int(b))
-            # print(diff)
             if diff < 1 or diff > 3:
-                # print("Unsafe", a, b)
                 total -= 1
                 break
 
@@ -34,9 +31,31 @@ def part_2():
     lines = parse_input(file_name)
     total = 0
 
+    def is_safe(levels: list[int]) -> bool:
+        j = [a - b for a, b in pairwise(levels)]
+        k = [True if 1 <= abs(i) <= 3 else False for i in j]
+
+        # Not in descending or ascending order - invalid
+        # There is a difference less than 1 or greater than 3 - invalid
+        if not (levels == sorted(levels) or levels == sorted(levels, reverse=True)) or not all(k):
+            return False
+
+        return True
+
+    for line in lines:
+        x = [int(c) for c in line.split(" ")]
+
+        if not is_safe(x):
+            for combo in combinations(x, len(x) - 1):
+                if is_safe(list(combo)):
+                    total += 1
+                    break
+        else:
+            total += 1
+
     print("Part 2: ", total)
 
 
 if __name__ == "__main__":
-    # part_1()
+    part_1()
     part_2()
