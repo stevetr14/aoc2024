f17d850c9c9be179da1e9d052a59b09e7b28f717
Steven Tran <stevetr14@hotmai.com>
2024-12-12 21:54:46 +1000

    Use cache decorator for Day 11

--- Day 11/solution.py
+++ Day 11/solution.py
@@ -1,6 +1,7 @@
 import math
 import time
 from collections import deque
+from functools import cache
 
 from utils import parse_input
 
@@ -18,34 +19,24 @@ def flatten_list(mixed_list):
     return flattened
 
 
-def split_stone(stone: str, iteration: int, max_iteration: int, memo: dict = None) -> int:
-    if memo is None:
-        memo = {}
-
+@cache
+def split_stone(stone: str, iteration: int, max_iteration: int) -> int:
     # Base case
     if iteration > max_iteration:
         return 1
 
-    # Check if the result is already computed
-    if (stone, iteration) in memo:
-        return memo[(stone, iteration)]
-
     count = 0
 
     if stone == "0":
-        count += split_stone("1", iteration + 1, max_iteration, memo)
+        count += split_stone("1", iteration + 1, max_iteration)
     elif len(stone) % 2 == 0:
         mid = len(stone) // 2
         # Use int to remove leading 0's from a string.
         left, right = int(stone[:mid]), int(stone[mid:])
-        count += split_stone(str(left), iteration + 1, max_iteration, memo)
-        count += split_stone(str(right), iteration + 1, max_iteration, memo)
+        count += split_stone(str(left), iteration + 1, max_iteration)
+        count += split_stone(str(right), iteration + 1, max_iteration)
     else:
-        # print(int(stone) * 2024)
-        count += split_stone(str(int(stone) * 2024), iteration + 1, max_iteration, memo)
-
-    # Store the result in memo
-    memo[(stone, iteration)] = count
+        count += split_stone(str(int(stone) * 2024), iteration + 1, max_iteration)
 
     return count
 
