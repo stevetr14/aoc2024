4ce44950eb284ca392d2666ddd14d5c82eeb028d
Steven Tran <steven.tran@deswik.com>
2024-12-13 15:05:08 +1000

    Day 7 setup

---
+++ Day 7/solution.py
@@ -0,0 +1,28 @@
+from utils import parse_input
+
+file_name = "test.txt"
+# file_name = "input.txt"
+
+
+def part_1():
+    lines = parse_input(file_name)
+
+    print(lines)
+
+    total = 0
+
+    print("Part 1: ", total)
+
+
+def part_2():
+    lines = parse_input(file_name)
+    print(lines)
+
+    total = 0
+
+    print("Part 2: ", total)
+
+
+if __name__ == "__main__":
+    part_1()
+    # part_2()
