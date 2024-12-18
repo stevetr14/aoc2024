01a144338dbd256013a15d8423774f3605314e71
Steven Tran <stevetr14@hotmai.com>
2024-12-18 22:26:46 +1000

    Day 18 part 2

--- utils.py
+++ utils.py
@@ -1,9 +1,5 @@
-import heapq
+from collections import deque
 from itertools import islice
-
-from typing import Any
-
-import numpy as np
 
 
 def parse_input(file_name: str, split_char: str = "\n") -> list[str]:
@@ -46,3 +42,25 @@ def find_position_in_matrix(matrix: list[list[str]], char: str) -> tuple[int, in
                 return i, j
 
     return None
+
+
+def bfs(maze: list[list[str]], start: tuple[int, int], end: tuple[int, int]) -> list[tuple[int, int]]:
+    queue = deque([(start, [start])])
+    seen = {start}
+    while queue:
+        (x, y), path = queue.popleft()
+
+        if (x, y) == end:
+            return path
+
+        for x2, y2 in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):  # directions
+            if (
+                0 <= x2 < len(maze[0]) and  # X-axis in range
+                0 <= y2 < len(maze) and   # y-axis
+                maze[y2][x2] != '#' and  # not a wall
+                (x2, y2) not in seen  # not visited
+            ):
+                queue.append(((x2, y2), path + [(x2, y2)]))
+                seen.add((x2, y2))
+
+    return []
